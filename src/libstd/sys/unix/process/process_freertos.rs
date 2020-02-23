3d58a347460638b2dffe0729ca961fbcd1bab92a
Markus Reiter <me@reitermark.us>
2020-02-23 09:04:43 +0100

    Add dummy `ExitStatus` struct.

--- src/libstd/sys/unix/process/process_freertos.rs
+++ src/libstd/sys/unix/process/process_freertos.rs
@@ -1,14 +1,20 @@
+use crate::fmt;
 use crate::io;
 use crate::sys::process::process_common::*;
 use crate::sys::{unsupported, unsupported_err, Void};
+
+use libc::c_int;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Command
 ////////////////////////////////////////////////////////////////////////////////
 
 impl Command {
-    pub fn spawn(&mut self, _default: Stdio, _needs_stdin: bool)
-                 -> io::Result<(Process, StdioPipes)> {
+    pub fn spawn(
+        &mut self,
+        _default: Stdio,
+        _needs_stdin: bool,
+    ) -> io::Result<(Process, StdioPipes)> {
         unsupported()
     }
 
@@ -40,4 +46,33 @@ impl Process {
     pub fn try_wait(&mut self) -> io::Result<Option<ExitStatus>> {
         match self.0 {}
     }
-  }
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+pub struct ExitStatus(Void);
+
+impl ExitStatus {
+    pub fn success(&self) -> bool {
+        match self.0 {}
+    }
+
+    pub fn code(&self) -> Option<i32> {
+        match self.0 {}
+    }
+
+    pub fn signal(&self) -> Option<i32> {
+        match self.0 {}
+    }
+}
+
+impl From<c_int> for ExitStatus {
+    fn from(a: c_int) -> ExitStatus {
+        unimplemented!()
+    }
+}
+
+impl fmt::Display for ExitStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.0 {}
+    }
+}
