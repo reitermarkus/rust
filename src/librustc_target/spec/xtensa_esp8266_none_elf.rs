77a7aee3fe99c522a62c13d6801d527d31e8992a
Markus Reiter <me@reitermark.us>
2020-05-02 03:21:37 +0200

    Allow executables on ESP8266.

--- src/librustc_target/spec/xtensa_esp8266_none_elf.rs
+++ src/librustc_target/spec/xtensa_esp8266_none_elf.rs
@@ -1,4 +1,4 @@
-use crate::spec::{abi::Abi, LinkerFlavor, PanicStrategy, Target, TargetOptions, TargetResult, RelocModel};
+use crate::spec::{abi::Abi, LinkerFlavor, LldFlavor, PanicStrategy, Target, TargetOptions, TargetResult, RelocModel};
 
 pub fn target() -> TargetResult {
     Ok(Target {
@@ -11,12 +11,13 @@ pub fn target() -> TargetResult {
         target_os: "freertos".to_string(),
         target_env: "newlib".to_string(),
         target_vendor: "espressif".to_string(),
-        linker_flavor: LinkerFlavor::Gcc,
+        linker_flavor: LinkerFlavor::Lld(LldFlavor::Ld),
 
         options: TargetOptions {
+            executables: true,
             target_family: Some("unix".to_string()),
             cpu: "esp8266".to_string(),
-            linker: Some("xtensa-lx106-elf-gcc".to_string()),
+            linker: Some("rust-lld".to_string()),
 
             max_atomic_width: Some(32),
 
