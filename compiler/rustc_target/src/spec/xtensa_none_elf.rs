27830bcdf55059fc47b8a134b58fc3e633d0f5a6
Scott Mabin <scott@mabez.dev>
2020-09-12 23:36:21 +0100

    Add some Xtensa targets. ESP32, ESP8266 and a generic Xtensa target.
    Enrich the Xtensa ESP32 and ESP8266 targets with target vendor, env and family
    
    Add some Xtensa targets

---
+++ compiler/rustc_target/src/spec/xtensa_none_elf.rs
@@ -0,0 +1,47 @@
+use crate::spec::{abi::Abi, LinkerFlavor, PanicStrategy, Target, TargetOptions, RelocModel};
+
+pub fn target() -> Target {
+    Target {
+        llvm_target: "xtensa-none-elf".to_string(),
+        data_layout: "e-m:e-p:32:32-i8:8:32-i16:16:32-i64:64-n32".to_string(),
+        arch: "xtensa".to_string(),
+        pointer_width: 32,
+
+        options: TargetOptions {
+            os: "none".to_string(),
+            env: String::new(),
+            vendor: String::new(),
+            
+            endian: "little".to_string(),
+            c_int_width: "32".to_string(),
+            max_atomic_width: Some(32),
+
+            executables: true,
+
+            linker: Some("xtensa-esp32-elf-gcc".to_string()),
+            linker_flavor: LinkerFlavor::Gcc,
+
+            // Because these devices have very little resources having an
+            // unwinder is too onerous so we default to "abort" because the
+            // "unwind" strategy is very rare.
+            panic_strategy: PanicStrategy::Abort,
+
+            // Similarly, one almost always never wants to use relocatable
+            // code because of the extra costs it involves.
+            relocation_model: RelocModel::Static,
+
+            emit_debug_gdb_scripts: false,
+
+            unsupported_abis: vec![
+                Abi::Stdcall,
+                Abi::Fastcall,
+                Abi::Vectorcall,
+                Abi::Thiscall,
+                Abi::Win64,
+                Abi::SysV64,
+            ],
+
+            ..Default::default()
+        },
+    }
+}
