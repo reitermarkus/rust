90ba40b135eca360ecab8771931ef63b02503801
Markus Reiter <me@reitermark.us>
2021-03-07 18:31:32 +0100

    Update formatting.

--- library/std/src/os/freertos/raw.rs
+++ library/std/src/os/freertos/raw.rs
@@ -1,11 +1,13 @@
 //! FreeRTOS-specific raw type definitions
 
 #![stable(feature = "raw_ext", since = "1.1.0")]
-#![rustc_deprecated(since = "1.8.0",
-                    reason = "these type aliases are no longer supported by \
-                              the standard library, the `libc` crate on \
-                              crates.io should be used instead for the correct \
-                              definitions")]
+#![rustc_deprecated(
+    since = "1.8.0",
+    reason = "these type aliases are no longer supported by \
+              the standard library, the `libc` crate on \
+              crates.io should be used instead for the correct \
+              definitions"
+)]
 #![allow(deprecated)]
 #![allow(missing_debug_implementations)]
 
